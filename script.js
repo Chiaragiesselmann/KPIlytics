b29db9272b64991c190cffc3c5dc4d6d3d56bb85
Chiaragiesselmann <chiara.giesselmann@iu-study.org>
2025-05-27 18:12:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function login() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  if (username === "admin" && password === "admin") {
+    alert("Login erfolgreich!");
+    // Weiterleitung zur Startseite hier möglich
+    // window.location.href = "startseite.html";
+  } else {
+    alert("Benutzername oder Passwort falsch!");
+  }
+}
 
